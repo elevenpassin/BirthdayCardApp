70009aac97b42b52ed5c70249b07ec878b7a0c87
Alice Kile <buoyantair@protonmail.com>
2024-03-05 20:46:10 +0100

    Add project

---
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.birthdaycardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(message = "Happy birthday sharon!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
+    )
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayCardAppTheme {
+        GreetingText(message = "Happy birthday Sharon!")
+    }
+}
